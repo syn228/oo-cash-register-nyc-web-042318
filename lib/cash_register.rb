bbf481839d0678e7b63813b53ae0ef05bef0c140
Aaron Na <syn228@nyu.edu>
2018-04-26 01:39:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,12 @@ class CashRegister
     @total = 0
   end
   
-  def total
-    @@total << stuff
+  # def total(total)
     
+  # end
+  
+  def add_item(title, price)
+    @title = title
+    @price = price
+    @@total += price
 end
